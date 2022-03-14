f90ecaee86d31143bbb4b085f48c374c70c59141
doriller <harvest767@yahoo.co.jp>
2022-03-14 09:03:42 +0000

    add validation AND status Column in tasksTable

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,6 +8,7 @@
             <thead>
                 <tr>
                     <th>ID</th>
+                    <th>ステータス</th>
                     <th>タスク</th>
                 </tr>
             </thead>
@@ -15,7 +16,8 @@
                 @foreach ($tasks as $task)
                 <tr>
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' =>$task->id]) !!}</td>
-                    <td>{{ $task->content }} </td>
+                    <td>{{ $task->status }}</td>
+                    <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
             </tbody>
